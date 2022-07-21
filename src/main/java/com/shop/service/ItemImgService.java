c5231d4450f5b5751cfe34cc842f7f4cdd4cf194
MinJu-A <pinokoiandmj@naver.com>
2022-07-21 19:15:07 +0900

    6.4 끝

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -1,13 +1,9 @@
 package com.shop.service;
 
-import com.shop.dto.ItemSearchDto;
-import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -71,6 +67,7 @@ public class ItemImgService {
 
         }
     }
+
 
 
 }
